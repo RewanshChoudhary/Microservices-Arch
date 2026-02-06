51a8969f0edbe9981ca898e4f2b4b4f8e864f065
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2026-02-06 12:51:25 +0530

    made change

--- src/main/java/com/example/Microservices/model/Accounts.java
+++ src/main/java/com/example/Microservices/model/Accounts.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
